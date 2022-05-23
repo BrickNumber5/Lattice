a66f44afc66ebad673e77aa61e3f0b58ec40fba3
BrickNumber5 <79104486+BrickNumber5@users.noreply.github.com>
2022-05-22 21:04:37 -0600

    Board Renderer

--- rewrite/gamelogic.js
+++ rewrite/gamelogic.js
@@ -2,9 +2,15 @@
 export class Board {
   #size;
   #nodes;
+  #ccos;
+  #csin;
   constructor(size) {
     this.#size = size;
     this.#nodes = new Map();
+    
+    this.#ccos = Math.cos(-Math.PI / 3);
+    this.#csin = Math.sin(-Math.PI / 3);
+    
     for (let u = 0; u < size; u++) {
       for (let v = 0; v < size - u; v++) {
         this.#nodes.set(u + v * size, new Node(this, u, v));
@@ -18,8 +24,40 @@ export class Board {
     return this.#nodes.get(u + v * this.#size);
   }
   
+  get size() {
+    return this.#size;
+  }
+  
+  boardToScreen(u, v, w, d) {
+    return {
+      x: w / 2 - (this.#size - 1) * d / 2 + u * d + v * d * this.#ccos,
+      y: w / 2 - (this.#size - 1) * d * this.#csin / 2 + v * d * this.#csin
+    };
+  }
+  
   draw(cnv, ctx, ticks) {
+    // Assumes the canvas is square
+    const d = cnv.width / (this.#size + 2);
     
+    ctx.fillStyle = "#222";
+    ctx.strokeStyle = "#ddd";
+    ctx.lineWidth = d / 12;
+    
+    ctx.beginPath();
+    for (let u = 0; u < this.#size - 1; u++) {
+      for (let v = 0; v < this.#size - u - 1; v++) {
+        const {x, y} = this.boardToScreen(u, v, cnv.width, d);
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + this.#ccos * d, y + this.#csin * d);
+        ctx.lineTo(x + d, y);
+        ctx.lineTo(x, y);
+      }
+    }
+    ctx.stroke();
+    
+    for (let [_, node] of this.#nodes) {
+      node.draw(cnv, ctx, ticks, d);
+    }
   }
 }
 
@@ -54,8 +92,16 @@ class Node {
     }
   }
   
-  draw(cnv, ctx, ticks) {
-    
+  place(player, range, spec) {
+    this.#piece = new Piece(this, player, range, spec);
+  }
+  
+  draw(cnv, ctx, ticks, d) {
+    ctx.beginPath();
+    const {x, y} = this.#board.boardToScreen(this.#u, this.#v, cnv.width, d);
+    ctx.arc(x, y, 7 * d / 24, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
   }
 }
 
@@ -72,4 +118,21 @@ class Piece {
     this.#spec = spec;
     this.#targetVector = null;
   }
+  
+  draw(dat, ctx, ticks) {
+    
+  }
+}
+
+let pTime = -1;
+export function loop(cTime) {
+  if (pTime == -1) {
+    pTime = cTime;
+    requestAnimationFrame(loop);
+    return;
+  }
+  let elapsedTime = cTime - pTime;
+  pTime = cTime;
+  
+  requestAnimationFrame(loop);
 }
--- rewrite/index.js
+++ rewrite/index.js
@@ -3,3 +3,10 @@ import * as UTIL from "./util.js";
 
 window.GAME = GAME;
 window.UTIL = UTIL;
+
+let b = new GAME.Board(9);
+let c = document.querySelector(".board");
+c.width = 600;
+c.height = 600;
+let ctx = c.getContext("2d");
+b.draw(c, ctx, 0);
